ecdd569abef465c544ca82d6f158ee861435ef8e
José C. Pernías <jcpernias@gmail.com>
2023-09-29 10:46:44 +0200

    Do not import sandwich in tests

--- tests/testthat/test-se.R
+++ tests/testthat/test-se.R
@@ -1,31 +1,30 @@
-library(sandwich)
-
 test_that("OLS standard errors", {
     x <- lm(mpg ~ disp, data = mtcars)
-    V <- vcov(x)
+    V <- stats::vcov(x)
     expect_equal(se(x), sqrt(diag(V)))
 })
 
 test_that("aliased OLS standard errors", {
     x <- lm(mpg ~ disp + I(disp + 1), data = mtcars)
-    V <- vcov(x, complete = TRUE)
+    V <- stats::vcov(x, complete = TRUE)
     expect_equal(se(x), sqrt(diag(V)))
 })
 
 test_that("HC0 standard errors", {
     x <- lm(mpg ~ disp, data = mtcars)
-    V <- vcovHC(x, type = "HC0")
+    V <- sandwich::vcovHC(x, type = "HC0")
     expect_equal(se(x, vcov = "HC0"), sqrt(diag(V)))
 })
 
 test_that("aliased HC0 standard errors", {
     x <- lm(mpg ~ disp + I(disp + 1), data = mtcars)
-    V <- .vcov.aliased(is.na(coef(x)), vcovHC(x, type = "HC0"))
+    V <- stats::.vcov.aliased(is.na(coef(x)),
+                              sandwich::vcovHC(x, type = "HC0"))
     expect_equal(se(x, vcov = "HC0"), sqrt(diag(V)))
 })
 
 test_that("HC standard errors", {
     x <- lm(mpg ~ disp, data = mtcars)
-    V <- vcovHC(x, type = "HC3")
+    V <- sandwich::vcovHC(x, type = "HC3")
     expect_equal(se(x, vcov = "HC"), sqrt(diag(V)))
 })
